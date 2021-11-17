79ebde00259fafe4db15c0a8a89ca80067afb2da
Emma Lyddon <elyddon@gmail.com>
2021-11-17 01:34:15 +0000

    Updated shoppingsite.py

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -49,8 +49,8 @@ def show_melon(melon_id):
 
     Show all info about a melon. Also, provide a button to buy that melon.
     """
-
-    melon = melons.get_by_id("meli")
+    # changed "meli" to melon_id
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -97,10 +97,35 @@ def add_to_cart(melon_id):
     #   dictionary keyed to the string "cart") if not
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
+    # - flash a success message    
+    if 'cart' in session:         
+        cart = session['cart']     
+    else:         
+        cart = session['cart'] = {}     
+        
+    # We could also do this with setdefault:     
+    # cart = session.setdefault("cart", {})      
+    # Add melon to cart - either increment the count (if melon already in cart)     
+    # or add to cart with a count of 1     
+    cart[melon_id] = cart.get(melon_id, 0) + 1
 
-    return "Oops! This needs to be implemented!"
+    # # if cart key in session, already have cart
+    # if "cart" in session:
+    #     # if melon already in cart, add one
+    #     if melon_id in session["cart"]:
+    #         ## not incrementing
+    #         session["cart"][melon_id] = session["cart"][melon_id] + 1
+    #         print("Add one to melon_id")
+    #     # else initialize new melon with value of 1
+    #     else:
+    #         session["cart"][melon_id] = 1
+    # else: 
+    #     # if no cart, create cart with melon_id = 1 
+    #     session["cart"] = {melon_id: 1}
+        
+    print(session["cart"])
+    # - redirect the user to the cart page
+    return redirect('/cart')
 
 
 @app.route("/login", methods=["GET"])
